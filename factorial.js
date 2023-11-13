07a62c367b5d4530ee3785ca1d6f04a8c3c6e3fd
medaVyshnavi <vyshnavivenkatesh@gmail.com>
2023-11-14 06:11:29 +0900

    recursion approach

--- factorial.js
+++ factorial.js
@@ -12,3 +12,15 @@ function getFactorial(n) {
 }
 
 console.log(getFactorial(7));
+
+// recursive approach big o notation is O(n)
+function getFact(n) {
+  if (n < 2) {
+    return 1;
+  }
+  return n * getFact(n - 1);
+}
+
+console.log(getFact(3)); //6
+console.log(getFact(5)); // 120
+console.log(getFact(4)); //24
--- fibonacchi.js
+++ fibonacchi.js
@@ -5,7 +5,24 @@ function getFibonacchi(n) {
   for (i = 2; i < n; i++) {
     arr[i] = arr[i - 1] + arr[i - 2];
   }
+  // OR
+  // for (i = 0; i < n; i++) {
+  //   arr[i + 2] = arr[i] + arr[i + 1];
+  // }
   return arr;
 }
 
 console.log(getFibonacchi(7));
+
+// Recursive Fibonacchi big O notation - o(2^n)
+
+function fibonacchi(n) {
+  if (n < 2) {
+    return n;
+  }
+  return fibonacchi(n - 1) + fibonacchi(n - 2);
+}
+
+console.log(fibonacchi(0));
+console.log(fibonacchi(1));
+console.log(fibonacchi(8));
