fd7321d04986e84689913de8820d42fe120690cf
medaVyshnavi <vyshnavivenkatesh@gmail.com>
2023-11-18 13:01:00 +0900

    binary search and bubble sort

--- binarySearch.js
+++ binarySearch.js
@@ -1,10 +1,44 @@
+//Binary sort => O(log n) - input value is halved in every iteration
+
 function binarySearch(arr, target) {
-  if (arr[arr.length / 2] === target) {
-    return arr[arr.length / 2];
+  let leftIndex = 0;
+  let rightIndex = arr.length - 1;
+
+  while (leftIndex <= rightIndex) {
+    let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+    if (arr[middleIndex] === target) {
+      return middleIndex;
+    }
+    if (target < arr[middleIndex]) {
+      rightIndex = middleIndex;
+    } else {
+      leftIndex = middleIndex;
+    }
   }
-  if (arr[arr.length / 2] > target) {
-    return arr[arr.length / 2];
-  }
+  return -1;
 }
 
-binarySearch([1, 3, 4, 5, 6, 7], 3);
+binarySearch([1, 3, 4, 5, 6], 3);
+
+// Binary search recursion => O(logn)
+
+function binarySearchRecursion(arr, target) {
+  return search(arr, target, 0, arr.length - 1);
+}
+
+function search(arr, target, left, right) {
+  let leftIndex = left;
+  let rightIndex = right;
+
+  if (leftIndex > rightIndex) return -1;
+
+  let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+  if (arr[middleIndex] === target) {
+    return middleIndex;
+  }
+  if (target < arr[middleIndex]) {
+    return search(arr, target, leftIndex, middleIndex);
+  } else {
+    return search(arr, target, middleIndex, leftIndex);
+  }
+}
---
+++ bubbleSort.js
@@ -0,0 +1,18 @@
+// Bubble sort => O(n^2)
+function bubbleSort(arr) {
+  let temp = 0;
+
+  for (i = 1; i <= arr.length - 1; i++) {
+    for (j = 0; j < i; j++) {
+      if (arr[i] < arr[j]) {
+        // temp = arr[j];
+        // arr[j] = arr[i];
+        // arr[i] = temp;
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+      }
+    }
+  }
+  return arr;
+}
+
+bubbleSort([8, 20, -2, -4, 6]);
