50541c968a86cf638032f84a26f85e769a6b60cb
medaVyshnavi <vyshnavivenkatesh@gmail.com>
2023-12-07 12:20:42 +0900

    PATTERNS

---
+++ PATTERNS/patterns7-12.js
@@ -0,0 +1,74 @@
+//   *
+//  ***
+// *****
+//*******
+
+function pattern7(n) {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n - i; j++) {
+      console.log(" ");
+    }
+    for (let j = 0; j < 2 * i + 1; j++) {
+      console.log("*");
+    }
+    for (let j = 0; j < n - i; j++) {
+      console.log(" ");
+    }
+    console.log("\n");
+  }
+}
+pattern7(4);
+
+//*********
+// *******
+//  *****
+//   ***
+//    *
+
+function pattern8(n) {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+      console.log(" ");
+    }
+    for (let j = 0; j < 2 * n - (2 * i + 1); j++) {
+      console.log("*");
+    }
+    for (let j = 0; j < i; j++) {
+      console.log(" ");
+    }
+    console.log("\n");
+  }
+}
+
+pattern8(4);
+
+//    *
+//   ***
+//  *****
+// *******
+//*********
+//*********
+// *******
+//  *****
+//   ***
+//    *
+function pattern9() {
+  pattern7(4);
+  pattern8(4);
+}
+
+pattern9();
+
+//*
+//**
+//***
+//****
+//*****
+//****
+//***
+//**
+//*
+
+function pattern10(n) {}
+
+pattern10(3);
