fe210fdb5b63a5934ce5520311a4b596b0cdf842
medaVyshnavi <vyshnavivenkatesh@gmail.com>
2023-12-10 21:29:03 +0900

    pattern11

--- PATTERNS/pattern11-14.js
+++ PATTERNS/pattern11-14.js
@@ -4,4 +4,28 @@
 //0 1 0 1
 //1 0 1 0 1
 
-function pattern11() {}
+function pattern11(n) {
+  for (let i = 0; i < n; i++) {
+    let start = i % 2 == 0 ? 1 : 0;
+    for (let j = 0; j <= i; j++) {
+      console.log(start);
+      start = 1 - start;
+    }
+    console.log("\n");
+  }
+}
+
+pattern11(5);
+
+//1      1
+//12    21
+//123  321
+//12344321
+
+function pattern12(n) {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+      console.log();
+    }
+  }
+}
