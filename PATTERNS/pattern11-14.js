d0cfc760f22e18e9b2464007d157afba66e4ff5c
medaVyshnavi <vyshnavivenkatesh@gmail.com>
2023-12-11 21:54:16 +0900

    characters

--- PATTERNS/pattern11-14.js
+++ PATTERNS/pattern11-14.js
@@ -23,9 +23,55 @@ pattern11(5);
 //12344321
 
 function pattern12(n) {
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < i; j++) {
-      console.log();
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      console.log(j);
     }
+    for (let j = 1; j <= 2 * n - 2 * i; j++) {
+      console.log(" ");
+    }
+    for (let j = i; j > 0; j--) {
+      console.log(j);
+    }
+    console.log("\n");
   }
 }
+
+pattern12(4);
+
+//1
+//2 3
+//4 5 6
+//7 8 9 10
+//11 12 13 14 15
+
+function pattern13(n) {
+  let number = 1;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j <= i; j++) {
+      console.log(number);
+      number++;
+    }
+    console.log("\n");
+  }
+}
+
+pattern13(5);
+
+// A
+// A B
+// A B C
+// A B C D
+// A B C D E
+
+function pattern14(n) {
+  for (let i = 0; i < n; i++) {
+    let char = 1;
+    for (let j = 0; j <= i; j++) {
+      console.log(String.fromCharCode(char + 64));
+      char++;
+    }
+    console.log("\n");
+  }
+}
+pattern14(5);
