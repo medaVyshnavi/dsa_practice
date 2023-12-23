102c349a1cf9ac3c51e219717eebf34c782ea16d
medaVyshnavi <vyshnavivenkatesh@gmail.com>
2023-12-23 13:43:53 +0900

    frequency

--- hashing/numberHashing.js
+++ hashing/numberHashing.js
@@ -30,3 +30,6 @@ console.log(frequency(arr2));
 console.log(frequency(arr2).keys().next().value);
 const newArr = Array.from(frequency(arr2));
 console.log(newArr[newArr.length - 1][0]); // 2- lowest frequency 1-highest frequency
+
+// Time Complexity: O(N), where N = size of the array. The insertion and retrieval operation in the map takes O(1) time.
+// Space Complexity:  O(N), where N = size of the array. It is for the map we are using.
