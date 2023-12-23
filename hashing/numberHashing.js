fc7ba6cc8f6517acf5594c2fc16aeaf46fef455d
medaVyshnavi <vyshnavivenkatesh@gmail.com>
2023-12-23 11:32:03 +0900

    hashing done

--- hashing/numberHashing.js
+++ hashing/numberHashing.js
@@ -11,4 +11,22 @@ function numberHashing(arr, n) {
 let arr = [1, 2, 1, 1];
 console.log(numberHashing(arr, 1));
 
-// size of
+// frequency of highest and lowest element
+function frequency(arr) {
+  let map2 = new Map();
+
+  for (let i = 0; i < arr.length; i++) {
+    map2.set(arr[i], (map2.get(arr[i]) || 0) + 1);
+  }
+
+  return new Map(
+    [...map2].sort(function (a, b) {
+      return a[1] - b[1];
+    })
+  );
+}
+let arr2 = [1, 2, 1, 1, 7, 8];
+console.log(frequency(arr2));
+console.log(frequency(arr2).keys().next().value);
+const newArr = Array.from(frequency(arr2));
+console.log(newArr[newArr.length - 1][0]); // 2- lowest frequency 1-highest frequency
